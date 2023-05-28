292ae9ca9f8f356046b507556d603c4e8c52970a
Md Hasib Askari <mdhsbaskari@gmail.com>
2023-05-29 01:29:45 +0600

    Version 1.0

---
+++ script.js
@@ -0,0 +1,12 @@
+const skipButton = document.getElementsByClassName("ytp-ad-skip-button");
+let addSkippedCount = 0;
+
+setInterval(function () {
+    // console.log("Working Start")
+    if (skipButton !== undefined &&  skipButton.length > 0) {
+        skipButton[0].click();
+        console.log("Add Skipped: "+ ++addSkippedCount);
+    }
+    // console.log("Working End")
+
+}, 1000);
